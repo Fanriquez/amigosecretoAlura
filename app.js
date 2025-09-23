e0a795b82e49c46ee342bc8379fe3be67b863e31
Fanriquez <144949584+Fanriquez@users.noreply.github.com>
2025-09-23 20:08:19 -0300

    Agregar amigos.js
    Agrega nombres a la lista Amigos, alerta espacio en blanco y eliminar espacios

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos []
+let amigos [];
+function= agregarAmigos(){
+  let imputAmigo = document.getElementById("amigo");
+  let amigo=inputAmigo.value.trim;
+
+  if (amigo===""){ 
+    alert ("Por favor, inserte un nombre.");
+    return;
+}
+amigos.push(amigo);
+inputAmigo.value = ""; }
